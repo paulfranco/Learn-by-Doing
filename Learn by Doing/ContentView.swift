1f4b900bd168776296fadaec0ce550ffde881f45
Paul Franco <mail@paulfran.co>
2020-07-14 23:13:57 -0700

    completed swift application

--- Learn by Doing/ContentView.swift
+++ Learn by Doing/ContentView.swift
@@ -8,13 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - PROPERTIES
+    var cards: [Card] = cardData
+    
+    
+    // MARK: - CONTENT
     var body: some View {
-        Text("Hello, world!").padding()
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(alignment: .center, spacing: 20) {
+                ForEach(cards) { item in
+                    CardView(card: item)
+                }
+            }
+            .padding(10)
+        }
     }
 }
 
+// MARK: - PREVIEW
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(cards: cardData)
+            .previewDevice("iPhone 11 Pro")
     }
 }
